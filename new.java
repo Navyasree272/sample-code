3795fa8c4cdc2e548ccd61a50b8462220a78bddb
unknown <chintanavyasree@gmail.com>
2021-09-28 17:21:33 +0530

    add new branch

--- new.java
+++ new.java
@@ -3,9 +3,7 @@ public class Helloworld
  public static void main(String[] args)
  {
 
-     system.out.ptintln(hellow world three 3)
-
-     system.out.ptintln(hellow world two)
+     system.out.ptintln(hello India ");
 	 }
 
 	 }
