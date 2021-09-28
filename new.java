096c68d806789730ebc9a2d523674545e64b22ea
unknown <chintanavyasree@gmail.com>
2021-09-28 17:39:32 +0530

    resolved issues

--- new.java
+++ new.java
@@ -3,7 +3,8 @@ public class Helloworld
  public static void main(String[] args)
  {
 
-     system.out.ptintln(hello India ");
+     system.out.ptintln(hello India 3");
+	 system.out.ptintln(hello India ");
 	 }
 
 	 }
